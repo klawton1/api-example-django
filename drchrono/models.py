14bf04b8f3721a18f6b5c7172935588432cdb1cb
klawton1 <klawton1@gmail.com>
2017-08-18 23:26:18 -0700

    model for checked in patients

--- drchrono/models.py
+++ drchrono/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class ArrivedPatient(models.Model):
+  first_name = models.CharField(max_length=200)
+  last_name = models.CharField(max_length=200)
+  ssn = models.CharField(max_length=50)
+  created_at = models.DateField(auto_now_add=True)
+  patient_id = models.IntegerField()
+  doctor_id = models.IntegerField()
+  chart_id = models.CharField(max_length=200)
+  appointment_id = models.CharField(max_length=200)
+  time_waiting = models.DurationField(default=datetime.timedelta(seconds=0))
+  seen_by_doctor = models.BooleanField(default=False)
