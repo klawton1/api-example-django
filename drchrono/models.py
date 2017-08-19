ec239c06263cfa853c42ad851d442bdc52d01259
klawton1 <klawton1@gmail.com>
2017-08-19 13:53:12 -0700

    migration for doctor profile

--- drchrono/models.py
+++ drchrono/models.py
@@ -7,9 +7,17 @@ class ArrivedPatient(models.Model):
   last_name = models.CharField(max_length=200)
   ssn = models.CharField(max_length=50)
   created_at = models.DateField(auto_now_add=True)
-  patient_id = models.IntegerField()
-  doctor_id = models.IntegerField()
+  patient_id = models.IntegerField(null=False, blank=False)
+  doctor_id = models.IntegerField(null=False, blank=False)
   chart_id = models.CharField(max_length=200)
   appointment_id = models.CharField(max_length=200)
   time_waiting = models.DurationField(default=datetime.timedelta(seconds=0))
   seen_by_doctor = models.BooleanField(default=False)
+
+class Profile(models.Model):
+  user = models.OneToOneField(User)
+  is_doctor = models.BooleanField(default=False)
+  doctor_id = models.IntegerField(null=False, blank=False)
+
+  def social(self):
+    return self.user.social_auth.first()
